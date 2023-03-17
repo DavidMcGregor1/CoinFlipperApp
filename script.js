68ac352a8b62c9058634115ad2c69713e0a45dce
David.McGregor <David.McGregor@hl.co.uk>
2023-03-17 13:12:00 +0000

    added click event listener for flip button and update stats method

---
+++ script.js
@@ -0,0 +1,30 @@
+let heads = 0;
+let tails = 0;
+let coin = document.querySelector(".coin");
+let flipButton = document.querySelector("#flip-button");
+let resetButton = document.querySelector("#reset-button");
+
+flipButton.addEventListener("click", () => {
+  let result = Math.floor(Math.random() * 2);
+  coin.style.animation = none;
+
+  if (result) {
+    setTimeout(function () {
+      coin.style.animation = "spin-heads 3s forwards";
+    }, 100);
+    heads++;
+  } else {
+    setTimeout(function () {
+      coin.style.animation = "spin-tails 3s forwards";
+    }, 100);
+    tails++;
+  }
+
+  setTimeout(updateStats, 3000);
+  disableButton();
+});
+
+function updateStats() {
+  document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
+  document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
+}
